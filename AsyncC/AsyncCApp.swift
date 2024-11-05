3a2da609b659364f6d1d1312cc2fec9332e92642
leewanjae <dhkswo1@gmail.com>
2024-11-05 23:59:06 +0900

    project init

---
+++ AsyncC/AsyncCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncCApp.swift
+//  AsyncC
+//
+//  Created by LeeWanJae on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
