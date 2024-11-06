6a9a3b754fb206402ec493e3d12c4e743abd3869
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-11-06 11:30:00 +0900

    [#1] - Application level에서 firebase configuration, firebase network test 완료

--- AsyncC/App/AsyncCApp.swift
+++ AsyncC/App/AsyncCApp.swift
@@ -5,13 +5,27 @@
 //  Created by LeeWanJae on 11/5/24.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        FirebaseApp.configure()
+    }
+}
 
 @main
 struct AsyncCApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+        ContentView()
     }
+  }
 }
