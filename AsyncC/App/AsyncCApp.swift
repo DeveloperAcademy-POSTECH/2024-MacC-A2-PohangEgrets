ecb25152337541974eb5125c54c41323da27f7dd
jin <ljin6381@naver.com>
2024-11-11 20:29:50 +0900

    [#16] - 오버레이 컴포넌트 구현 (NSPopover)

--- AsyncC/App/AsyncCApp.swift
+++ AsyncC/App/AsyncCApp.swift
@@ -5,15 +5,38 @@
 //  Created by LeeWanJae on 11/5/24.
 //
 
-
 import SwiftUI
 import FirebaseCore
 
-
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarItem: NSStatusItem?
+    var popover: NSPopover!
     
     func applicationDidFinishLaunching(_ notification: Notification) {
+        // Firebase configure
         FirebaseApp.configure()
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusBarItem?.button {
+            button.image = NSImage(systemSymbolName: "star", accessibilityDescription: "Status Bar Icon")
+            button.action = #selector(togglePopover)
+        }
+        
+        popover = NSPopover()
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusBarItem?.button {
+            if popover.isShown {
+                popover.performClose(nil)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
+        }
     }
 }
 
@@ -24,8 +47,10 @@ struct AsyncCApp: App {
 
 
   var body: some Scene {
-    WindowGroup {
-        ContentView()
-    }
+      // Remove WindowGroup to prevent the main window from appearing
+      // and only display the Status Bar item.
+      Settings {
+          EmptyView()
+      }
   }
 }
