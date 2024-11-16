29ba1a84e43b7f1378e93d2f2193f83574114f97
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-11-17 08:04:45 +0900

    [#37] - CreateTeamview 수정하기

--- AsyncC/Source/Presentation/CreateTeamView/CreateTeamView.swift
+++ AsyncC/Source/Presentation/CreateTeamView/CreateTeamView.swift
@@ -15,7 +15,8 @@ struct CreateTeamView: View {
     var body: some View {
         VStack(alignment: .center) {
             Text("팀 생성하기")
-            .padding(EdgeInsets(top: 0, leading: 12, bottom: 8, trailing: 0))
+                .font(Font.system(size: 16, weight: .semibold))
+                .padding(EdgeInsets(top: 30, leading: 12, bottom: 0, trailing: 0))
             TextField("팀 이름을 입력하세요", text:  $teamName)
                 .textFieldStyle(.roundedBorder)
                 .padding(EdgeInsets(top: 16, leading: 24, bottom: 31, trailing: 24))
@@ -34,7 +35,7 @@ struct CreateTeamView: View {
                     router.showHUDWindow()
                 }
                 .disabled(teamName.isEmpty)
-                .foregroundStyle(Color.accentColor)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12))
                 
             }
             .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12))
