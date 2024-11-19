af7400a3e67588503ca956b42dc51e5d63a36ef7
ing <ing@ingui-MacBookPro.local>
2024-11-20 03:31:26 +0900

    Emoticon Model 수정

--- AsyncC/Source/Domain/Entity/Emoticon.swift
+++ AsyncC/Source/Domain/Entity/Emoticon.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct Emoticon {
+    var sender: String //보낸이 UUID
     var receiver: String // 받는이 UUID
     var emoticon: emoticonOption
-    var sender: String //보낸이 UUID
+    let timestamp: Date
+    var isAcknowledged: Bool = false
     
     enum emoticonOption: String {
-        case getHelp
-        case giveHelp
-        case yes
-        case no
+        case getHelp = "도움요청"
+        case giveHelp = "도움제안"
     }
 }
 
