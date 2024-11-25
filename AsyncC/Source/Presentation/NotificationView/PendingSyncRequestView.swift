6ed8d311704b102c9cd926ffef6100cc9a3c1b6b
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-11-25 23:24:13 +0900

    [#82] - 간격 맞추고 배경색 수정

--- AsyncC/Source/Presentation/NotificationView/PendingSyncRequestView.swift
+++ AsyncC/Source/Presentation/NotificationView/PendingSyncRequestView.swift
@@ -19,44 +19,44 @@ struct PendingSyncRequestView: View {
     @ObservedObject var viewModel = SyncRequestNotificationViewModel()
     
     var body: some View {
-        VStack(spacing: 10) {
-            if amSender {
-                Text("\(recipientName)의 수락을 기다리는 중")
-                    .font(Font.system(size: 16, weight: .medium))
-                    .padding(EdgeInsets(top: 12, leading: 0, bottom: 0, trailing: 0))
-            } else {
-                Text("\(senderName)의 도움요청")
-                    .font(Font.system(size: 16, weight: .medium))
-                    .padding(EdgeInsets(top: 12, leading: 0, bottom: 0, trailing: 0))
-            }
-            Text("10초 안에 수락을 안하면 자동으로 거절됩니다")
-                .font(Font.system(size: 12, weight: .light))
-                .padding(EdgeInsets(top: 0, leading: 12, bottom: 12, trailing: 12))
-            Text("\(viewModel.secondsLeft)")
-                .font(Font.system(size: 24, weight: .heavy))
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
-            if !amSender {
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 4)
-                                .frame(height: 24)
-                                .foregroundStyle(Color.white)
-                            Text("도움 수락하기")
-                                .font(Font.system(size: 12, weight: .medium))
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(.regularMaterial)
+                .frame(maxWidth: 300)
+            VStack(spacing: 10) {
+                if amSender {
+                    Text("\(recipientName)의 수락을 기다리는 중")
+                        .font(Font.system(size: 16, weight: .medium))
+                } else {
+                    Text("\(senderName)의 도움요청")
+                        .font(Font.system(size: 16, weight: .medium))
                 }
-                .padding(12)
+                Text("10초 안에 수락을 안하면 자동으로 거절됩니다")
+                    .font(Font.system(size: 12, weight: .light))
+                    .padding(EdgeInsets(top: 0, leading: 12, bottom: 12, trailing: 12))
+                Text("\(viewModel.secondsLeft)")
+                    .font(Font.system(size: 24, weight: .heavy))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                if !amSender {
+                    HStack {
+                        Button {
+                            viewModel.acceptSyncRequest(to: senderID)
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 4)
+                                    .frame(height: 30)
+                                    .foregroundStyle(Color.white)
+                                Text("도움 수락하기")
+                                    .font(Font.system(size: 12, weight: .medium))
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+//                    .padding(12)
+                }
             }
+            .padding(EdgeInsets(top: 20, leading: 18, bottom: 20, trailing: 18))
         }
-        .padding()
-        .background(Color.gray.opacity(0.9))
-        .cornerRadius(10)
-//        .frame(minWidth: 300, minHeight: 130)
         .fixedSize(horizontal: true, vertical: true)
         .onAppear {
             viewModel.router = router
