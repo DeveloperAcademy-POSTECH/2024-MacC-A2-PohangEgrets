fc93aafdf7dc20be44ec653a9ae63d66df89520a
leewanjae <dhkswo1@gmail.com>
2024-11-25 15:34:07 +0900

    [#70] 안쓰는 코드 삭제

--- AsyncC/Source/Presentation/MainStatusView/Components/AppIconBoxContentView.swift
+++ AsyncC/Source/Presentation/MainStatusView/Components/AppIconBoxContentView.swift
@@ -5,28 +5,6 @@
 //  Created by LeeWanJae on 11/22/24.
 //
 
-//import SwiftUI
-//
-//struct AppIconBoxContentView: View {
-//    @ObservedObject var viewModel: MainStatusViewModel
-//    var key: String
-//    
-//    var body: some View {
-//        HStack(spacing: 4) {
-//            
-//            ForEach(viewModel.appTrackings[key] ?? [], id:\.self) { appName in
-//                Image("\(appName)")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width: 36, height: 36)
-//                    .opacity(viewModel.getOpacity(appName: appName, apps: viewModel.appTrackings[key] ?? []))
-//            }
-//        }
-//        .padding(.leading, 12)
-//        .frame(maxWidth: .infinity, alignment: .leading)
-//    }
-//}
-
 import SwiftUI
 
 struct AppIconBoxContentView: View {
